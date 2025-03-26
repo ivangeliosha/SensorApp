5c8e6440ff18afc4bf34dca779b03bfe5c7e5cb7
ivan <gopmiy36@gmail.com>
2025-03-27 00:11:21 +0300

    рабочая версия с некорректной работой ExceptionHandler
    Signed-off-by: ivan <gopmiy36@gmail.com>

--- src/main/java/ru/maslennikov/thirdProject/SensorApp/controllers/MeasurementController.java
+++ src/main/java/ru/maslennikov/thirdProject/SensorApp/controllers/MeasurementController.java
@@ -61,7 +61,7 @@ public class MeasurementController {
     MeasurementErrorResponse response = new MeasurementErrorResponse(
         e.getMessage(),
         System.currentTimeMillis()
-    );//sdvsfvsv
+    );//sdvsfvsvedwdfwdfw
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
